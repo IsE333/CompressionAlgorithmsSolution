0426060bbcb2b02785af11f47b47dbbd2af2ceb7
IsE333 <77530401+IsE333@users.noreply.github.com>
2025-12-20 20:36:51 +0300

    RLE algorithm added

--- CompressConsoleApp/Program.cs
+++ CompressConsoleApp/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace CompressConsoleApp
+﻿using CompressionAlgorithms;
+using System.Text;
+
+namespace CompressConsoleApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            ExampleRLE("aabbbccddddee");
+        }
+
+        static void ExampleRLE(string text)
+        {
+            Console.WriteLine("Run Length Encoding!");
+            var input = Encoding.UTF8.GetBytes(text);
+            var rle = new RunLengthEncoding();
+            var compressed = rle.Compress(input);
+
+            Console.WriteLine($"Original text:      {text}");
+            Console.WriteLine($"Original bytes:     {BitConverter.ToString(input).Replace("-", " ")}");
+            Console.WriteLine($"Compressed bytes:   {BitConverter.ToString(compressed).Replace("-", " ")}");
+
+            var decompressed = rle.Decompress(compressed);
+            var decompressedText = Encoding.UTF8.GetString(decompressed);
+            Console.WriteLine();
+            Console.WriteLine($"Decompressed bytes: {BitConverter.ToString(decompressed).Replace("-", " ")}");
+            Console.WriteLine($"Decompressed text:  {decompressedText}");
         }
     }
 }
---
+++ CompressionAlgorithms/RunLengthEncoding.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompressionAlgorithms
+{
+    public class RunLengthEncoding : IAlgorithm
+    {
+        public byte[] Compress(byte[] data)
+        {
+            var compressed = new List<byte>();
+            byte prev = data[0];
+            int count = 1;
+            for (int i = 1; i <= data.Length; i++)
+            {
+                byte current = i == data.Length ? data[i - 1]: data[i];
+                if (current != prev || i == data.Length) 
+                {
+                    compressed.Add((byte)count);
+                    compressed.Add(prev);
+                    count = 1;
+                } else {                     
+                    count++;
+                }
+                prev = current;
+            }
+            return compressed.ToArray();
+        }
+
+        public byte[] Decompress(byte[] compressedData)
+        {
+            var decompressed = new List<byte>();
+            for (int i = 0; i < compressedData.Length; i += 2)
+            {
+                byte count = compressedData[i];
+                byte value = compressedData[i + 1];
+                decompressed.AddRange(Enumerable.Repeat(value, count));
+            }
+            return decompressed.ToArray();
+        }
+    }
+}
